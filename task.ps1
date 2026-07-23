e6f7ce559f06046a364ecfd69bcf8b0ce8b18bc1
1ntact <verbaivan9@gmail.com>
2026-07-24 00:59:49 +0200

    1

--- task.ps1
+++ task.ps1
@@ -1,1 +1,13 @@
-# Write your code here
+$regions_with_correct_size = @()
+
+$files = Get-ChildItem -Path "data" -Filter "*.json"
+
+foreach ($file in $files) {
+    $content = Get-Content -Path $file.FullName | ConvertFrom-Json
+    if ($content.Name -contains "Standard_B2pts_v2")
+    {
+        $regions_with_correct_size += $file.BaseName
+    }
+}
+
+ConvertTo-Json -InputObject $regions_with_correct_size | Out-File -FilePath "result.json"
